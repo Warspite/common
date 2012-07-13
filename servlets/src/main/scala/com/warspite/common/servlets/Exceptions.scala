6d53cadca524be91aabe5a52b8577b47f236c344
Daniel Franzen <daniel.b.franzen@gmail.com>
2012-07-13 18:21:41 +0200

    Stepped to 1.4.7. Added auth failure exception to servlets.

--- servlets/src/main/scala/com/warspite/common/servlets/Exceptions.scala
+++ servlets/src/main/scala/com/warspite/common/servlets/Exceptions.scala
@@ -1,5 +1,6 @@
 package com.warspite.common.servlets;
 import com.warspite.common.servlets.RestVerb._
+import com.warspite.common.servlets.sessions.Session
 
 class ClientReadableException(msg: String, val clientMessage: String, inner: Throwable) extends Exception(msg, inner) {
   def this(msg: String, clientMessage: String) = this(msg, clientMessage, null);
@@ -7,6 +8,5 @@ class ClientReadableException(msg: String, val clientMessage: String, inner: Thr
 }
 
 class UnsupportedRestVerbException(verb: RestVerb, servletName: String) extends ClientReadableException("Servlet " + servletName + " received a " + verb + " request, which it does not support.", "Sorry! This servlet does not support " + verb + " requests.", null) {}
-
-
 class NotJsonifiableTypeException(obj: Any) extends Exception("Jsonification failed. Don't know how to jsonify objects of type " + obj.asInstanceOf[AnyRef].getClass) {}
+class AuthorizationFailureException(session: Session) extends ClientReadableException("Unauthorized servlet request by session " + session + " intercepted.", "Oops! It seems you were trying to perform an unauthorized action. That's most unfortunate :(") {}
